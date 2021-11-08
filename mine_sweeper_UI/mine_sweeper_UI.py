56eeba337a22ffb209126b36bf39bb27c7cddf57
Luis Diego Araya Campos <luisdiegoarayacampos@hotmail.com>
2021-11-07 23:54:45 -0600

    Se agrega la clase Ui_Game y las funciones matrix_creation y game_window

--- mine_sweeper_UI/mine_sweeper_UI.py
+++ mine_sweeper_UI/mine_sweeper_UI.py
@@ -189,6 +189,112 @@ def mine_window(max_mines):
     return mines
 
 
+class Ui_Game(object):
+    def setupUi(self, MainWindow, n, m):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(653, 434)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.checkBox.setFont(font)
+        self.checkBox.setIconSize(QtCore.QSize(16, 16))
+        self.checkBox.setObjectName("checkBox")
+        self.verticalLayout.addWidget(self.checkBox)
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.button_grid(n, m)
+        self.verticalLayout.addLayout(self.gridLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        MainWindow.insertToolBarBreak(self.toolBar)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 653, 27))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.menubar.setFont(font)
+        self.menubar.setObjectName("menubar")
+        self.menuReinicio = QtWidgets.QMenu(self.menubar)
+        self.menuReinicio.setObjectName("menuReinicio")
+        MainWindow.setMenuBar(self.menubar)
+        self.actionReinicio_suave = QtWidgets.QAction(MainWindow)
+        self.actionReinicio_suave.setObjectName("actionReinicio_suave")
+        self.actionReinicio_fuerte = QtWidgets.QAction(MainWindow)
+        self.actionReinicio_fuerte.setObjectName("actionReinicio_fuerte")
+        self.menuReinicio.addAction(self.actionReinicio_suave)
+        self.menuReinicio.addAction(self.actionReinicio_fuerte)
+        self.menubar.addAction(self.menuReinicio.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Buscaminas"))
+        self.checkBox.setText(_translate("MainWindow", "Colocar bandera"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.menuReinicio.setTitle(_translate("MainWindow", "Reinicio"))
+        self.actionReinicio_suave.setText(_translate(
+                                                     "MainWindow",
+                                                     "Reinicio suave"
+                                                     )
+                                          )
+        self.actionReinicio_fuerte.setText(_translate(
+                                                      "MainWindow",
+                                                      "Reinicio fuerte"
+                                                      )
+                                           )
+
+    def button_grid(self, n, m):
+        self.matrix = matrix_creation(n, m)
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+                       QtGui.QPixmap("blank.png"),
+                       QtGui.QIcon.Normal,
+                       QtGui.QIcon.Off
+                       )
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        for a in range(n):
+            for b in range(m):
+                self.matrix[a][b] = QtWidgets.QPushButton(self.centralwidget)
+                self.matrix[a][b].setFont(font)
+                self.matrix[a][b].setText("")
+                self.matrix[a][b].setIcon(icon)
+                self.matrix[a][b].setIconSize(QtCore.QSize(38, 38))
+                self.gridLayout.addWidget(self.matrix[a][b], a, b, 1, 1)
+
+
+def matrix_creation(n, m):
+    matrix = []
+
+    for _ in range(n):
+        matrix.append([])
+
+    for a in range(n):
+        for _ in range(m):
+            matrix[a].append([])
+
+    return matrix
+
+
+def game_window(n, m):
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_Game()
+    ui.setupUi(MainWindow, n, m)
+    MainWindow.show()
+    app.exec_()
+
+
 class Menus():  # estos metodos deberian estar en una clase?
 
     def dim_window():
