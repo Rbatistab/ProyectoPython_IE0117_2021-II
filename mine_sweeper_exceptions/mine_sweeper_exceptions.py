92718b13535aa9ed67a6a98327f4cd15cf293131
russbatisbarrrco <russbatisbarrrco@gmail.com>
2021-11-26 16:54:59 -0600

    Removing unsused files

--- mine_sweeper_exceptions/mine_sweeper_exceptions.py
+++
@@ -1,9 +0,0 @@
-class TerminateMineSweeper(Exception):
-    pass
-
-# Necesitamos un exception para el reset:
-# El manejo del programa se hace desde el controlador, este consume el UI,
-# pero cuando un usuario toca un boton de reset
-# el boton lo toca en el UI y este no tiene acceso al controlador. Para hacer
-# un reset (full o soft) necesitamos una
-# exception que ordene el reset
